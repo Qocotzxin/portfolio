685f3ca8f3f61aec0b5f455b831b25e9ba22cabd
Cristian Etchebarne <cetchebarne@convene.com>
2022-09-09 15:35:02 -0300

    Added error and not found pages to avoid build errors

---
+++ pages/500.tsx
@@ -0,0 +1,29 @@
+import { ApiResponse } from "@models/api";
+import { HygraphData } from "@models/hygraph";
+import axios from "axios";
+import type { GetStaticPropsContext, NextPage } from "next";
+
+const Error: NextPage = () => {
+  return <div data-testid="Error">Error</div>;
+};
+
+export async function getStaticProps({ locale }: GetStaticPropsContext) {
+  let response;
+  try {
+    response = await axios.get<ApiResponse<HygraphData>>(
+      `${process.env.NEXT_PUBLIC_VERCEL_URL}/api/hygraph?locale=${locale}`
+    );
+  } catch (e: unknown) {
+    // Call logging service.
+  }
+
+  const data = response ? response.data : null;
+
+  return {
+    props: {
+      hygraphData: data?.data,
+    },
+  };
+}
+
+export default Error;
---
+++ tests/routes/500.spec.tsx
@@ -0,0 +1,52 @@
+import ErrorPage, { getStaticProps } from "@pages/500";
+import { render, screen } from "@utils/test-utils";
+import axios from "axios";
+
+jest.mock("axios", () => ({
+  get: jest.fn(() => ({ data: { data: { test: "test" } } })),
+}));
+
+const ORIGINAL_ENV_VARS = process.env;
+
+beforeEach(() => {
+  jest.resetModules();
+  process.env = { ...ORIGINAL_ENV_VARS };
+});
+
+afterAll(() => {
+  process.env = ORIGINAL_ENV_VARS;
+});
+
+describe("About", () => {
+  it("Should render Error page.", () => {
+    render(<ErrorPage />);
+    expect(screen.getByTestId("Error")).toBeInTheDocument();
+  });
+
+  it("Should call hygraph endpoint with the correct parameters and return the result when executing getStaticProps.", async () => {
+    process.env.NEXT_PUBLIC_VERCEL_URL = "https://test-api";
+    const result = await getStaticProps({ locale: "en" });
+    expect(axios.get).toHaveBeenCalledWith(
+      "https://test-api/api/hygraph?locale=en"
+    );
+    expect(result).toEqual({
+      props: {
+        hygraphData: { test: "test" },
+      },
+    });
+  });
+
+  it("Should return data as null when getStaticProps is called but there is an error.", async () => {
+    (axios.get as jest.MockedFunction<typeof axios.get>).mockImplementation(
+      () => {
+        throw new Error("Error");
+      }
+    );
+    const result = await getStaticProps({ locale: "en" });
+    expect(result).toEqual({
+      props: {
+        hygraphData: undefined,
+      },
+    });
+  });
+});
